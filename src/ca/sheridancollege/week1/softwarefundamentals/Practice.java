cbd1efb76ea41da6382f34828be2496ee926420f
Kulvi <Kulvi@192.168.0.21>
2019-05-24 09:28:31 -0400

    fourth commit

---
+++ src/ca/sheridancollege/week1/softwarefundamentals/Practice.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.sheridancollege.week1.softwarefundamentals;
+
+/**
+ *
+ * @author Kulvi
+ */
+public class Practice {
+    public static void main(String[] args){
+    System.out.println("hello");
+    }
+}
